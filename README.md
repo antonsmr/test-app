9033eeb15056e8001cee74c01e0cf551d954a8ce
anton@digitalsuits.co <anton@digitalsuits.co>
2019-06-12 15:21:00 +0300

    with deploy

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Example ENVs
+export REACT_APP_NODE_ENV=development
+export FIREBASE_DEPLOY_KEY=
+export FIREBASE_PROJECT=
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App';
 import configureStore from './store/configureStore';
-import * as serviceWorker from './serviceWorker';
 
 const store = configureStore();
 
 ReactDOM.render(<App store={store} />, document.getElementById('root'));
-
-serviceWorker.unregister();
